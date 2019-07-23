fb1de180fd6672c2e719eceaa15f8e86a91ca2bc
Matt <mbb670@gmail.com>
2019-07-23 06:18:59 -0400

    xl sixe and severla updates
    fixed sidebar and othe px widths. added xl sixe

--- js/scripts-min.js
+++ js/scripts-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){var e=$("#vert-type"),t=$("#test");TweenMax.set(e,{y:-460,x:180,rotation:-90,transformOrigin:"center center"}),TweenMax.to(t,100,{x:1e3})});
+$(document).ready(function(){var e=$("#vert-type"),t=$("#test");TweenMax.set(e,{y:-460,x:180,rotation:-90,transformOrigin:"center center"}),TweenMax.set(t,{alpha:50}),TweenMax.to(t,500,{x:1e4})});
--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ var type1 = $('#vert-type');
 var test = $('#test');
 
 TweenMax.set(type1, {y:-460, x:180, rotation:-90, transformOrigin:"center center"});
-TweenMax.to(test, 100, {x:1000});
+TweenMax.set(test, {alpha:50});
+TweenMax.to(test, 500, {x:10000});
 });
