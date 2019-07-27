846c9689a1b885efcd85b3967b786f8718665092
Matt <mbb670@gmail.com>
2019-07-27 17:16:26 -0400

    Attermpt at fixing sidebar

--- js/scripts-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var e=$("#vert-type"),t=$("#test");TweenMax.set(e,{y:-460,x:180,rotation:-90,transformOrigin:"center center"}),TweenMax.set(t,{alpha:50}),TweenMax.to(t,500,{x:1e4})});
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-$( document ).ready(function() {
-
-var type1 = $('#vert-type');
-var test = $('#test');
-
-TweenMax.set(type1, {y:-460, x:180, rotation:-90, transformOrigin:"center center"});
-TweenMax.set(test, {alpha:50});
-TweenMax.to(test, 500, {x:10000});
-});
+// $( document ).ready(function() {
+//
+// var type1 = $('#vert-type');
+// var test = $('#test');
+//
+// TweenMax.set(type1, {y:-460, x:180, rotation:-90, transformOrigin:"center center"});
+// TweenMax.set(test, {alpha:50});
+// TweenMax.to(test, 500, {x:10000});
+// });
