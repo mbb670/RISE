26edc92f19b0f6cbaef5519b08977c16ad0d1007
Matt <mbb670@gmail.com>
2019-08-07 18:39:08 -0400

    Mainly Complete
    Click on links does not change back to burger. Document Ready still screwing things up

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-    var $burger = $("#burger-icon");
+    var $burger = $("#burger-container");
     var $nav = $('nav[data-nav]');
     var $navA = $('nav[data-nav] a');
     var isVisible = false;
@@ -37,10 +37,36 @@ $( document ).ready(function() {
     });
 
 
-var type1 = $('#vert-type');
-var test = $('#test');
 
-TweenMax.set(type1, {y:-460, x:180, rotation:-90, transformOrigin:"center center"});
-TweenMax.set(test, {alpha:50});
-TweenMax.to(test, 500, {x:10000});
+var $topLine = $('#topLine');
+var $middleLine = $('#middleLine');
+var $bottomLine = $('#bottomLine');
+var $burgerContainer = $('#burger-container');
+var animationSpeed = 0.25;
+var ifClicked = false;
+
+function burgerClick(){
+    console.log("clicks");
+    if(ifClicked === false){
+        TweenMax.to($topLine,animationSpeed,{rotation:45, y:14, transformOrigin: "50%, 50%"});
+        TweenMax.to($middleLine,animationSpeed,{alpha:0, scaleX:0, transformOrigin: "50%, 50%"});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:-45, y:-14, transformOrigin: "50%, 50%"});
+        ifClicked = true;
+    }else{
+        TweenMax.to($topLine,animationSpeed,{rotation:0, y:0});
+        TweenMax.to($middleLine,animationSpeed,{alpha:1, scaleX:1});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:0, y:0});
+        ifClicked = false;
+    }
+}
+
+$burgerContainer.on("click", burgerClick);
+
+
+// var type1 = $('#vert-type');
+// var test = $('#test');
+//
+// TweenMax.set(type1, {y:-460, x:180, rotation:-90, transformOrigin:"center center"});
+// TweenMax.set(test, {alpha:50});
+// TweenMax.to(test, 500, {x:10000});
 });
